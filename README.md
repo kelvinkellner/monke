726230448e3253b20a27b1afc9611125f01b8c41
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-01-21 20:55:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# the_genie2
+
+A Discord bot that replies with a picture of a monkey.
+Made to annoy a friend, of course, as all good things are.
---
+++ bot.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = process.env.BOT_TOKEN; //set to: process.env.BOT_TOKEN for Heroku deployment, set to: config.LOCAL_TOKEN for local testing
+  
+client.on('message', message => { //this event is fired, whenever the bot sees a new message
+    if (message.isMemberMentioned(client.user)) { //we check, whether the bot is mentioned, client.user returns the user that the client is logged in as
+        message.channel.send('no');
+        message.channel.send('monke');
+        message.channel.send('',{files: ['./assets/monke.png']});
+    }
+});
+
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(token); // BOT_TOKEN is the Client Secret
