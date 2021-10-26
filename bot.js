7189d80a2016ceb2df934576b1d59f323e770c20
Kelvin Kellner <kelvinkellner123@gmail.com>
2021-10-26 18:43:34 -0400

    Start basics of playing sound to voice channel

--- bot.js
+++ bot.js
@@ -9,9 +9,22 @@ client.on('ready', () => {
 
 client.on('message', message => { //this event is fired, whenever the bot sees a new message
     if (message.isMemberMentioned(client.user)) { //we check, whether the bot is mentioned, client.user returns the user that the client is logged in as
+        // Join the same voice channel of the author of the message
         message.channel.send('no');
         message.channel.send('monke');
         message.channel.send('',{files: ['./assets/monke.png']});
+        if (message.member.voice.channel) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play('./assets/monke-noises.mp3');
+            dispatcher.on('start', () => {
+                console.log('audio.mp3 is now playing!');
+            });
+            dispatcher.on('finish', () => {
+                console.log('audio.mp3 has finished playing!');
+                connection.disconnect();
+            });
+            dispatcher.on('error', console.error);
+        }
     }
 });
 
